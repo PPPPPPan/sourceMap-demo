cebe5a26438ffdbb5cfda8d6cef0d50a2add9569
PPPPPPan <891523948@qq.com>
2024-07-25 15:24:25 +0800

    add:recover sourcemap

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './assets/main.css'
-
+import ErrorStackParser from "error-stack-parser"
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { findCodeBySourceMap } from './utils/index.ts'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.config.errorHandler = (err,vm) => {
+  const errorStack = ErrorStackParser.parse(err as Error)
+  findCodeBySourceMap(errorStack[0])
+}
 
 app.mount('#app')
