0bc92029d29ee903842461ecb5e21f28b4f58e55
PPPPPPan <891523948@qq.com>
2024-07-26 16:40:24 +0800

    style:App.vue

--- src/App.vue
+++ src/App.vue
@@ -77,12 +77,12 @@ nav a:first-of-type {
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
     font-size: 1rem;
+    /* text-align: left;
+    margin-left: -1rem;
 
     padding: 1rem 0;
-    margin-top: 1rem;
+    margin-top: 1rem; */
   }
 }
 </style>
